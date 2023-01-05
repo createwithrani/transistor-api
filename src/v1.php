9c52b22ab866aedf01e3c18e29ba9123af853c14
Aurooba Ahmed <hey@aurooba.com>
2023-01-04 19:33:13 -0700

    possible verbose version of the api

---
+++ src/v1.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CreateWithRani\Transistor;
+
+use CreateWithRani\Transistor\Transistor;
+
+/**
+ * A more verbose wrapper for the Transistor API v1
+ *
+ * @package CreateWithRani\Transistor
+ * @author Aurooba Ahmed <aurooba@auroobamakes.com>
+ * @version 1.0.0
+ */
+
+class V1 extends Transistor {
+
+
+
+}
